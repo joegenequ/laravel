1b03c6dfda07d544e7505351242816b88141ace8
joegenequ <joegenequ@gmail.com>
2024-08-31 04:41:04 +0000

    Added comment on URL forceRootURL

--- laravel-docker/routes/web.php
+++ laravel-docker/routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\URL;   //Added this to allow forceRoot URL to get from .env APP_URL
 
 $url = config('app.url');
-URL::forceRootUrl($url);
+URL::forceRootUrl($url);  //Added this to allow forceRoot URL to get from .env APP_URL
 
 Route::view('/', 'home')->name('home');
 Route::view('contact', 'contact')->name('contact');
