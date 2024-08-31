6980d29b614320af0457bc67432e55b533eb3412
joegenequ <joegenequ@gmail.com>
2024-08-31 04:34:38 +0000

    Added forceRootUrl so that route helper works

--- laravel-docker/routes/web.php
+++ laravel-docker/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+
+$url = config('app.url');
+URL::forceRootUrl($url);
 
 Route::view('/', 'home')->name('home');
 Route::view('contact', 'contact')->name('contact');
