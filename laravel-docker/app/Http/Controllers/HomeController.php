ce77e67d762a178fd14057288e30f07423b21b50
joegenequ <joegenequ@gmail.com>
2024-08-31 05:59:33 +0000

    MVC: Model, Controller and View with Foreach 02

--- laravel-docker/app/Http/Controllers/HomeController.php
+++ laravel-docker/app/Http/Controllers/HomeController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = ['Category 1', 'Category 2']; 
+        $allCategories = DB::table('categories')->get(); 
  
         return view('home', ['categories' => $allCategories]); 
     }
--- laravel-docker/resources/views/home.blade.php
+++ laravel-docker/resources/views/home.blade.php
@@ -108,7 +108,7 @@
                                 <div class="col-sm-6">
                                     <ul class="list-unstyled mb-0">
                                     @foreach($categories as $category) 
-                                        <li><a href="#!">{{ $category }}</a></li>
+                                        <li><a href="#!">{{ $category->name }}</a></li> 
                                     @endforeach
                             </div>
                         </div>
