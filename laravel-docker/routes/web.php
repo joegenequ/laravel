aa68eade75674f969d955daf8af34f32c869f7cf
joegenequ <joegenequ@gmail.com>
2024-08-31 04:57:18 +0000

    Added forum post discussing  forceRoot URL

--- laravel-docker/routes/web.php
+++ laravel-docker/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;   //Added this to allow forceRoot URL to get from .env APP_URL
+//https://github.com/laravel/framework/issues/18613#issuecomment-703707939
 
 $url = config('app.url');
 URL::forceRootUrl($url);  //Added this to allow forceRoot URL to get from .env APP_URL
