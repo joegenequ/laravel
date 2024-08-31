5eb6a33b8b4a71d6a49b1d669fcbecf64c685e1b
joegenequ <joegenequ@gmail.com>
2024-08-31 07:30:14 +0000

    Route Model Binding , Link to the Page

--- laravel-docker/routes/web.php
+++ laravel-docker/routes/web.php
@@ -10,3 +10,5 @@ URL::forceRootUrl($url);  //Added this to allow forceRoot URL to get from .env A
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home'); 
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
+
+Route::get('posts/{post}', [\App\Http\Controllers\PostController::class, 'show'])->name('post.show'); 
