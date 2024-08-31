d3d20cd97d2d937d656bcd805b7c535de7ffa9f4
joegenequ <joegenequ@gmail.com>
2024-08-31 04:05:11 +0000

    DB Structure, Migrations and .env Config

--- laravel-docker/resources/views/layouts/app.blade.php
+++ laravel-docker/resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->        
-        <link href="https://orange-space-parakeet-q7g55qg55p4369x4-80.app.github.dev/css/styles.css" rel="stylesheet" /> 
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />  
     </head>
     <body>
         <!-- Responsive navbar-->
@@ -19,10 +19,9 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li> 
+                <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('contact') }}">Contact</a></li>                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
                     </ul>
                 </div>
             </div>
